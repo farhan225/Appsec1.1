ed7d91f9e4e90015c916ff6aefc10432eaf3188b
Farhan Abdullah <farhan225@gmail.com>
2021-02-17 21:09:57 -0500

    Fixing Crash1.gft

--- giftcardreader.c
+++ giftcardreader.c
@@ -183,6 +183,14 @@ struct this_gift_card *gift_card_reader(FILE *input_fd) {
 		/* JAC: Why aren't return types checked? */
 		fread(&ret_val->num_bytes, 4,1, input_fd);
 
+        //validating the value of num_byte to be positive and not negative
+        if (ret_val->num_bytes < 0) {
+
+            printf (" Negative value exiting \n");
+            exit(0);
+
+        }
+
 		// Make something the size of the rest and read it in
 		ptr = malloc(ret_val->num_bytes);
 		fread(ptr, ret_val->num_bytes, 1, input_fd);
